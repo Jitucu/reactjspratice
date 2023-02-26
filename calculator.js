2ed85474d3979e7a0f1df8fbcab16c1fd5611ce0
Jitucu <jitendra.jnv14@gmail.com>
2023-02-26 12:12:18 +0530

    added

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let num=document.querySelector("#B1")
+num.addEventListener("click",()=>{
+    let val=document.getElementById("B1")
+    var c=val.innerHTML
+    console.log(c);
+})
+let num2=document.querySelector("#B2")
+num2.addEventListener("click",()=>{
+    var val2=document.getElementById("B2")
+    var d=val2.innerHTML
+    console.log(d);
+})
+let res=document.querySelector("#eq")
+res.addEventListener("click",()=>{
+        d
+})
+
