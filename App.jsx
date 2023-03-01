ac43569d2839cd867e7c86f989983d1c6b121d00
Jitucu <120647912+Jitucu@users.noreply.github.com>
2023-03-01 22:31:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const App = ()=>{
+    return (
+        <>
+        
+        <div>App</div>
+        <h1>Hi</h1>
+        <h1>Bye</h1>
+        <h2>React</h2>
+        </>
+    )
+}
+export default App
