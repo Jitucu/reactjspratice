6b88eb4a1f0baff4ceb94d15d94cc21417ce2425
Jitucu <120647912+Jitucu@users.noreply.github.com>
2023-03-09 22:56:57 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,7 +4,10 @@ import Care from './Components/Care';
 import Cla from './Components/Cla';
 import Cla2 from './Components/Cla2';
 import Prop from './Components/Prop';
-import Photo from './Components/Image.jpg'
+import Photo from './Components/Image.jpg';
+import JSON from './Components/Movies.json';
+import Details from './Components/Details';
+import "./global.css";
 
 const App=()=>{
     return(
@@ -16,7 +19,8 @@ const App=()=>{
       {/*<Prop data={143} Boolean={true} data2={null}/>
       <Prop value={{username:"jitendra", password:123456789}}/>
       <img src="https://c4.wallpaperflare.com/wallpaper/469/771/781/ktm-1290-super-duke-r-motorcycle-sports-wallpaper-preview.jpg" alt="" />*/}
-      <img src={Photo} alt="" />
+      {/*<img src={Photo} alt="" />*/}
+      <Details payload={JSON}/>
       </>
 
     )
---
+++ Details.jsx
@@ -0,0 +1,249 @@
+import React from "react"
+
+const Details=(props)=>{
+   
+   return(
+    
+    <div className="mainBlock">
+        <div className="childBlock">
+            <img src={props.payload[0].Movieposter} alt="" />
+            <h1>{props.payload[0].Moviename}</h1>
+            <h1>{props.payload[0].Director}</h1>
+            <h1>{props.payload[0].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[1].Movieposter} alt="" />
+            <h1>{props.payload[1].Moviename}</h1>
+            <h1>{props.payload[1].Director}</h1>
+            <h1>{props.payload[1].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[2].Movieposter} alt="" />
+            <h1>{props.payload[2].Moviename}</h1>
+            <h1>{props.payload[2].Director}</h1>
+            <h1>{props.payload[2].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[3].Movieposter} alt="" />
+            <h1>{props.payload[3].Moviename}</h1>
+            <h1>{props.payload[3].Director}</h1>
+            <h1>{props.payload[3].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[4].Movieposter} alt="" />
+            <h1>{props.payload[4].Moviename}</h1>
+            <h1>{props.payload[4].Director}</h1>
+            <h1>{props.payload[4].ReleaseDate}</h1>
+       </div>
+            <div className="childBlock">
+            <img src={props.payload[5].Movieposter} alt="" />
+            <h1>{props.payload[5].Moviename}</h1>
+            <h1>{props.payload[5].Director}</h1>
+            <h1>{props.payload[5].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[6].Movieposter} alt="" />
+            <h1>{props.payload[6].Moviename}</h1>
+            <h1>{props.payload[6].Director}</h1>
+            <h1>{props.payload[6].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[7].Movieposter} alt="" />
+            <h1>{props.payload[7].Moviename}</h1>
+            <h1>{props.payload[7].Director}</h1>
+            <h1>{props.payload[7].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[8].Movieposter} alt="" />
+            <h1>{props.payload[8].Moviename}</h1>
+            <h1>{props.payload[8].Director}</h1>
+            <h1>{props.payload[8].ReleaseDate}</h1>
+            </div>
+            < div className="childBlock">
+            <img src={props.payload[9].Movieposter} alt="" />
+            <h1>{props.payload[9].Moviename}</h1>
+            <h1>{props.payload[9].Director}</h1>
+            <h1>{props.payload[9].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[10].Movieposter} alt="" />
+            <h1>{props.payload[10].Moviename}</h1>
+            <h1>{props.payload[10].Director}</h1>
+            <h1>{props.payload[10].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[11].Movieposter} alt="" />
+            <h1>{props.payload[11].Moviename}</h1>
+            <h1>{props.payload[11].Director}</h1>
+            <h1>{props.payload[11].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[12].Movieposter} alt="" />
+            <h1>{props.payload[12].Moviename}</h1>
+            <h1>{props.payload[12].Director}</h1>
+            <h1>{props.payload[12].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[13].Movieposter} alt="" />
+            <h1>{props.payload[13].Moviename}</h1>
+            <h1>{props.payload[13].Director}</h1>
+            <h1>{props.payload[13].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[14].Movieposter} alt="" />
+            <h1>{props.payload[14].Moviename}</h1>
+            <h1>{props.payload[14].Director}</h1>
+            <h1>{props.payload[14].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[15].Movieposter} alt="" />
+            <h1>{props.payload[15].Moviename}</h1>
+            <h1>{props.payload[15].Director}</h1>
+            <h1>{props.payload[15].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[16].Movieposter} alt="" />
+            <h1>{props.payload[16].Moviename}</h1>
+            <h1>{props.payload[16].Director}</h1>
+            <h1>{props.payload[16].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[17].Movieposter} alt="" />
+            <h1>{props.payload[17].Moviename}</h1>
+            <h1>{props.payload[17].Director}</h1>
+            <h1>{props.payload[17].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[18].Movieposter} alt="" />
+            <h1>{props.payload[18].Moviename}</h1>
+            <h1>{props.payload[18].Director}</h1>
+            <h1>{props.payload[18].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[19].Movieposter} alt="" />
+            <h1>{props.payload[19].Moviename}</h1>
+            <h1>{props.payload[19].Director}</h1>
+            <h1>{props.payload[19].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[20].Movieposter} alt="" />
+            <h1>{props.payload[20].Moviename}</h1>
+            <h1>{props.payload[20].Director}</h1>
+            <h1>{props.payload[20].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[21].Movieposter} alt="" />
+            <h1>{props.payload[21].Moviename}</h1>
+            <h1>{props.payload[21].Director}</h1>
+            <h1>{props.payload[21].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[22].Movieposter} alt="" />
+            <h1>{props.payload[22].Moviename}</h1>
+            <h1>{props.payload[22].Director}</h1>
+            <h1>{props.payload[22].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[23].Movieposter} alt="" />
+            <h1>{props.payload[23].Moviename}</h1>
+            <h1>{props.payload[23].Director}</h1>
+            <h1>{props.payload[23].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[24].Movieposter} alt="" />
+            <h1>{props.payload[24].Moviename}</h1>
+            <h1>{props.payload[24].Director}</h1>
+            <h1>{props.payload[24].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[25].Movieposter} alt="" />
+            <h1>{props.payload[25].Moviename}</h1>
+            <h1>{props.payload[25].Director}</h1>
+            <h1>{props.payload[25].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[26].Movieposter} alt="" />
+            <h1>{props.payload[26].Moviename}</h1>
+            <h1>{props.payload[26].Director}</h1>
+            <h1>{props.payload[26].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[27].Movieposter} alt="" />
+            <h1>{props.payload[27].Moviename}</h1>
+            <h1>{props.payload[27].Director}</h1>
+            <h1>{props.payload[27].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[28].Movieposter} alt="" />
+            <h1>{props.payload[28].Moviename}</h1>
+            <h1>{props.payload[28].Director}</h1>
+            <h1>{props.payload[28].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[29].Movieposter} alt="" />
+            <h1>{props.payload[29].Moviename}</h1>
+            <h1>{props.payload[29].Director}</h1>
+            <h1>{props.payload[29].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[30].Movieposter} alt="" />
+            <h1>{props.payload[30].Moviename}</h1>
+            <h1>{props.payload[30].Director}</h1>
+            <h1>{props.payload[30].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[31].Movieposter} alt="" />
+            <h1>{props.payload[31].Moviename}</h1>
+            <h1>{props.payload[31].Director}</h1>
+            <h1>{props.payload[31].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[32].Movieposter} alt="" />
+            <h1>{props.payload[32].Moviename}</h1>
+            <h1>{props.payload[32].Director}</h1>
+            <h1>{props.payload[32].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[33].Movieposter} alt="" />
+            <h1>{props.payload[33].Moviename}</h1>
+            <h1>{props.payload[33].Director}</h1>
+            <h1>{props.payload[33].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[34].Movieposter} alt="" />
+            <h1>{props.payload[34].Moviename}</h1>
+            <h1>{props.payload[34].Director}</h1>
+            <h1>{props.payload[34].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[35].Movieposter} alt="" />
+            <h1>{props.payload[35].Moviename}</h1>
+            <h1>{props.payload[35].Director}</h1>
+            <h1>{props.payload[35].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[36].Movieposter} alt="" />
+            <h1>{props.payload[36].Moviename}</h1>
+            <h1>{props.payload[36].Director}</h1>
+            <h1>{props.payload[36].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[37].Movieposter} alt="" />
+            <h1>{props.payload[37].Moviename}</h1>
+            <h1>{props.payload[37].Director}</h1>
+            <h1>{props.payload[37].ReleaseDate}</h1>
+            </div>
+            <div className="childBlock">
+            <img src={props.payload[38].Movieposter} alt="" />
+            <h1>{props.payload[38].Moviename}</h1>
+            <h1>{props.payload[38].Director}</h1>
+            <h1>{props.payload[38].ReleaseDate}</h1>
+            </div> 
+        </div>
+   
+  
+    
+   )
+
+}
+export default Details
