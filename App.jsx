25d248f431480348fb9114420fc3abb1dd2ff9ea
Jitucu <120647912+Jitucu@users.noreply.github.com>
2023-03-13 20:09:09 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,34 +1,16 @@
-import React from "react"
-import Nav from './Components/Nav';
-import Care from './Components/Care';
-import Cla from './Components/Cla';
-import Cla2 from './Components/Cla2';
-import Prop from './Components/Prop';
-import Photo from './Components/Image.jpg';
-import JSON from './Components/Movies.json';
-import Details from './Components/Details';
-import "./global.css";
-import Course from './Course';
 
-const App=()=>{
+import React from 'react'
+import {Component} from 'react'
+
+class App extends Component{
+ state={
+  username:"Rahul"
+ }
+  render()
+  {
     return(
-        <>
-      {/*<Nav girl = "Shriya"/>
-      <Care boy = "Rahul"/>
-      <Cla item ="Pen" />
-      <Cla2 place ="Bangalore"/>*/}
-      {/*<Prop data={143} Boolean={true} data2={null}/>
-      <Prop value={{username:"jitendra", password:123456789}}/>
-      <img src="https://c4.wallpaperflare.com/wallpaper/469/771/781/ktm-1290-super-duke-r-motorcycle-sports-wallpaper-preview.jpg" alt="" />*/}
-      {/*<img src={Photo} alt="" />*/}
-      {/*<Details payload={JSON}/>*/}
-      <Course details={{courseName:"Java Full Stack",
-                        courseAvail: true,
-                        courseDuration:"2 Months"}
-    }/>
-      </>
-
+      <h1>{this.state.username}</h1>
     )
-
+  }
 }
 export default App
