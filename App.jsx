19c42972e05a967346b2acf26ec58b111918f06e
Jitucu <120647912+Jitucu@users.noreply.github.com>
2023-03-15 19:47:50 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,16 +1,41 @@
+import React, { Component } from 'react'
+import Counter from './Components/Counter.css'
 
-import React from 'react'
-import {Component} from 'react'
-
-class App extends Component{
- state={
-  username:"Rahul"
- }
-  render()
+export default class App extends Component {
+  constructor()
   {
+    super()
+    this.state={
+      count:0
+    }
+    
+
+  }
+  handleIncrement=()=>{
     return(
-      <h1>{this.state.username}</h1>
+      this.setState({count:this.state.count+1})
+    )
+  }
+handleDecrement=()=>{
+  this.setState({count:this.state.count-1})
+}
+handleReset=()=>{
+  return(
+    this.setState({count:0})
+  )
+}
+  render() {
+    return (
+      <>
+      <div className='mainblock'>
+     <h1>{this.state.count}</h1>
+     </div>
+     <div className='subblock'>
+     <button onClick={this.handleIncrement}>+ Increment</button>
+     <button onClick={this.handleDecrement}>- Decrement</button>
+     <button onClick={this.handleReset}>RESET</button>
+     </div>
+     </>
     )
   }
 }
-export default App
